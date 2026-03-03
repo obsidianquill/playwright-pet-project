a31b0f48d9cc73ffd7f65978376f32fcecd0784e
Danuta Kashtanova <danutakashtanova@Danutas-MacBook-Air.local>
2026-03-03 19:12:13 +0200

    updated login.spec test

--- tests/smoke/login.spec.js
+++ tests/smoke/login.spec.js
@@ -1,12 +1,12 @@
 import {test, expect} from '@playwright/test';
+import { LoginPage } from '../../pages/LoginPage';
 
 test ('Login', async ({ page }) => {
-    await page.goto('/');
-    const usernameField = page.locator('#user-name');
-    const passwordField = page.locator('#password');
-    const loginButton = page.locator('#login-button')
-    await usernameField.fill('visual_user');
-    await passwordField.fill('secret_sauce');
-    await loginButton.click();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.usernameInput.fill('visual_user');
+    await loginPage.passwordInput.fill('secret_sauce');
+    await loginPage.loginButton.click();
+    await expect(page).toHaveURL(/inventory.html/);
 }
 )
